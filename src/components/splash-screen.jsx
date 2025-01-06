69bc1499e53aed963ab0a88dc324e8f70e898222
Francisco J <franlsym@users.noreply.github.com>
2025-01-06 20:52:06 +0000

    Update splash screen and language styles, and add copilot instructions

--- src/components/splash-screen.jsx
+++ src/components/splash-screen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import logoImage from '../assets/logo-144x144.png';
 import '../style/splash-screen.css';
 
 const SplashScreen = () => {
@@ -11,7 +12,7 @@ const SplashScreen = () => {
         <div className="logo-container">
           <div className="morph-background"></div>
           <img 
-            src="/icons/android/android-launchericon-144-144.png" 
+            src={logoImage}
             alt={t('appName')}
             className="logo"
           />
